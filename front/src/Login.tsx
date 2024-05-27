5bdb9637ee50b4b8ce0ab65c0e50b0823084a6be
Jae Won Bae <49405805+JaewonB37@users.noreply.github.com>
2024-05-27 11:57:27 +0900

    Update Login.tsx

--- front/src/Login.tsx
+++ front/src/Login.tsx
@@ -15,6 +15,7 @@ import Typography from '@mui/joy/Typography';
 import Stack from '@mui/joy/Stack';
 import DarkModeRoundedIcon from '@mui/icons-material/DarkModeRounded';
 import LightModeRoundedIcon from '@mui/icons-material/LightModeRounded';
+import BadgeRoundedIcon from '@mui/icons-material/BadgeRounded';
 import { useNavigate } from 'react-router-dom';
 
 import GoogleIcon from './GoogleIcon';
@@ -70,62 +71,145 @@ export default function SignIn({ onSignIn }: { onSignIn: () => void }) {
   };
 
   return (
-    <CssVarsProvider defaultMode="light" disableTransitionOnChange>
+    <CssVarsProvider defaultMode="dark" disableTransitionOnChange>
       <CssBaseline />
       <GlobalStyles
         styles={{
           ':root': {
-            '--Form-maxWidth': '400px',
+            '--Form-maxWidth': '800px',
             '--Transition-duration': '0.4s',
           },
         }}
       />
       <Box
-        sx={{
+        sx={(theme) => ({
+          width: { xs: '100%', md: '50vw' },
+          transition: 'width var(--Transition-duration)',
+          transitionDelay: 'calc(var(--Transition-duration) + 0.1s)',
+          position: 'relative',
+          zIndex: 1,
           display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          height: '100vh',
-          backgroundColor: '#F0EAFB',
-        }}
+          justifyContent: 'flex-end',
+          backdropFilter: 'blur(12px)',
+          backgroundColor: 'rgba(255 255 255 / 0.2)',
+          [theme.getColorSchemeSelector('dark')]: {
+            backgroundColor: 'rgba(19 19 24 / 0.4)',
+          },
+        })}
       >
         <Box
           sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            minHeight: '100dvh',
             width: '100%',
-            maxWidth: '400px',
-            bgcolor: 'background.paper',
-            boxShadow: 1,
-            borderRadius: 2,
-            p: 3,
-            textAlign: 'center',
+            px: 2,
           }}
         >
-          <img src={Logo} alt="하모나이즈" style={{ marginBottom: '16px', width: '50px', height: '50px' }} />
-          <Typography component="h1" level="h3" sx={{ mb: 1.5 }}>
-            로그인
-          </Typography>
-          <Typography level="body-sm" sx={{ mb: 3 }}>
-            아이디와 비밀번호를 입력하여 로그인하세요.
-          </Typography>
-          <form onSubmit={handleSubmit}>
-            <FormControl required sx={{ mb: 2 }}>
-              <FormLabel>아이디</FormLabel>
-              <Input type="email" name="email" placeholder="아이디 입력" />
-            </FormControl>
-            <FormControl required sx={{ mb: 2 }}>
-              <FormLabel>비밀번호</FormLabel>
-              <Input type="password" name="password" placeholder="비밀번호 입력" />
-            </FormControl>
-            <Stack direction="row" justifyContent="space-between" sx={{ mb: 2 }}>
-              <Checkbox size="sm" label="Remember me" name="persistent" />
-              <Link level="title-sm" href="#replace-with-a-link">
-                Forgot your password?
-              </Link>
+          <Box
+            component="header"
+            sx={{
+              py: 3,
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}
+          >
+            <Box sx={{ gap: 2, display: 'flex', alignItems: 'center' }}>
+              <IconButton color="primary" size="sm">
+                <img src={Logo} alt="하모나이즈" style={{ width: '50px', height: '50px' }} />
+              </IconButton>
+              <Typography level="title-lg">Harmonize</Typography>
+            </Box>
+            <ColorSchemeToggle />
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              my: 'auto',
+              py: 2,
+              pb: 5,
+              display: 'flex',
+              flexDirection: 'column',
+              gap: 2,
+              width: 400,
+              maxWidth: '100%',
+              mx: 'auto',
+              borderRadius: 'sm',
+              '& form': {
+                display: 'flex',
+                flexDirection: 'column',
+                gap: 2,
+              },
+              [`& .MuiFormLabel-asterisk`]: {
+                visibility: 'hidden',
+              },
+            }}
+          >
+            <Stack gap={4} sx={{ mb: 2 }}>
+              <Stack gap={1}>
+                <Typography component="h1" level="h3">
+                  Sign in
+                </Typography>
+                <Typography level="body-sm">
+                  New to company?{' '}
+                  <Link href="#replace-with-a-link" level="title-sm">
+                    Sign up!
+                  </Link>
+                </Typography>
+              </Stack>
+              <Button
+                variant="soft"
+                color="neutral"
+                fullWidth
+                startDecorator={<GoogleIcon />}
+              >
+                Continue with Google
+              </Button>
             </Stack>
-            <Button type="submit" fullWidth sx={{ mb: 2 }}>
-              로그인
-            </Button>
-          </form>
+            <Divider
+              sx={(theme) => ({
+                [theme.getColorSchemeSelector('light')]: {
+                  color: { xs: '#FFF', md: 'text.tertiary' },
+                },
+              })}
+            >
+              or
+            </Divider>
+            <Stack gap={4} sx={{ mt: 2 }}>
+              <form onSubmit={handleSubmit}>
+                <FormControl required>
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" name="email" />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" name="password" />
+                </FormControl>
+                <Stack gap={4} sx={{ mt: 2 }}>
+                  <Box
+                    sx={{
+                      display: 'flex',
+                      justifyContent: 'space-between',
+                      alignItems: 'center',
+                    }}
+                  >
+                    <Checkbox size="sm" label="Remember me" name="persistent" />
+                    <Link level="title-sm" href="#replace-with-a-link">
+                      Forgot your password?
+                    </Link>
+                  </Box>
+                  <Button type="submit" fullWidth>
+                    Sign in
+                  </Button>
+                </Stack>
+              </form>
+            </Stack>
+          </Box>
+          <Box component="footer" sx={{ py: 3 }}>
+            <Typography level="body-xs" textAlign="center">
+              © Your company {new Date().getFullYear()}
+            </Typography>
+          </Box>
         </Box>
       </Box>
     </CssVarsProvider>
