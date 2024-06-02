7634e454189ca1b48bf0e5be713f2797bbfcafa4
Jae Won Bae <49405805+JaewonB37@users.noreply.github.com>
2024-06-02 14:53:19 +0900

    AddSinger implement

--- front/src/pages/singer/AddSinger.tsx
+++ front/src/pages/singer/AddSinger.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import { useState, ChangeEvent } from 'react';
 import { CssVarsProvider } from '@mui/joy/styles';
 import CssBaseline from '@mui/joy/CssBaseline';
 import Box from '@mui/joy/Box';
@@ -7,10 +8,34 @@ import Breadcrumbs from '@mui/joy/Breadcrumbs';
 import Link from '@mui/joy/Link';
 import HomeRoundedIcon from '@mui/icons-material/HomeRounded';
 import ChevronRightRoundedIcon from '@mui/icons-material/ChevronRightRounded';
+import Button from '@mui/joy/Button';
+import Input from '@mui/joy/Input';
+import Select from '@mui/joy/Select';
+import Option from '@mui/joy/Option';
 
 import './singer.css';
 
+const GENDERLIST = ['남성', '여성', '혼성']
+const TYPELIST = ['솔로', '듀오', '그룹']
+
 export default function AddSinger() {
+
+  // 이미지 표시용으로 임시로 넣어둠
+  const [profilePicture, setProfilePicture] = useState<string | null>(null);
+
+  const handleProfilePictureChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = () => {
+        if (typeof reader.result === 'string') {
+          setProfilePicture(reader.result);
+        }
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
     <CssVarsProvider disableTransitionOnChange>
       <CssBaseline />
@@ -55,10 +80,55 @@ export default function AddSinger() {
             </Typography>
 
             <div className="singer">
-
-
-
-            </div>  
+              <div className='horizontal'>
+                <div>
+                  <div className={(profilePicture) ? "none" : ""}>
+                    <input type="file" id="profilePicture" accept="image/*" onChange={handleProfilePictureChange} className="inputfile" />
+                    <label htmlFor="profilePicture" className="plus-button">+<span className='name'>프로필</span></label>
+                  </div>              
+                  {profilePicture && (
+                    <label htmlFor="profilePicture"><img src={profilePicture} alt="프로필" className='preview' /></label>
+                  )}
+                </div>
+                <div style={{ margin: "0 0 0 12px" }}>
+                  <Input type="text" placeholder="가수 이름 입력" sx={{ width: 400 }}/>
+                </div>
+              </div>
+              <div className='section'>
+                <p className='sectionTitle'>가수 정보</p>
+                <div className='item'>
+                  <span>성별</span>
+                  <Select placeholder="성별 선택" sx={{ width: 180 }}>
+                  {
+                    GENDERLIST.map((gender, index) => (
+                      <Option key={index} value={gender}>{gender}</Option>
+                    ))
+                  }
+                  </Select>
+                </div>
+                <div className='item'>
+                  <span>유형</span>
+                  <Select placeholder="유형 선택" sx={{ width: 180 }}>
+                  {
+                    TYPELIST.map((type, index) => (
+                      <Option key={index} value={type}>{type}</Option>
+                    ))
+                  }
+                  </Select>
+                </div>
+                <div className='item'>
+                  <span>활동년대</span>
+                  <Input type="text" placeholder="활동년대 입력" sx={{ width: 180 }}/>
+                </div>
+                <div className='item'>
+                  <span>국적</span>
+                  <Input type="text" placeholder="국적 입력" sx={{ width: 180 }}/>
+                </div>
+              </div>
+              <div className='action'>
+                <Button variant="solid" sx={{ width: 120, height: 40 }}>가수 등록</Button>
+              </div>
+            </div>
 
           </Box>
         </Box>
